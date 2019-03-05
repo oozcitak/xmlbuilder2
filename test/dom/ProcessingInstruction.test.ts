cbfb72aa3d019648797e192f0eabeca940fd1d86
Özgür Özçıtak <oozcitak@outlook.com>
2019-03-05 20:39:51 +0300

    Complete ProcessingInstruction tests

---
+++ test/dom/ProcessingInstruction.test.ts
@@ -0,0 +1,32 @@
+import $$ from '../TestHelpers'
+
+describe('ProcessingInstruction', function () {
+
+  const doc = $$.dom.createDocument('myns', 'root')
+  const node = doc.createProcessingInstruction('program', 'instruction')
+  if (doc.documentElement) {
+    doc.documentElement.appendChild(node)
+  }
+
+  test('constructor()', function () {
+    expect(node.nodeType).toBe(7)
+    expect(node.nodeName).toBe('program')
+    expect(node.target).toBe('program')
+    expect(node.data).toBe('instruction')
+  })
+
+  test('cloneNode()', function () {
+    const clonedNode = <any>node.cloneNode()
+    expect(clonedNode).not.toBe(node)
+    expect(clonedNode.nodeType).toBe(7)
+    expect(clonedNode.nodeName).toBe('program')
+    expect(clonedNode.target).toBe('program')
+    expect(clonedNode.data).toBe('instruction')
+  })
+
+  test('isEqualNode()', function () {
+    const node2 = doc.createProcessingInstruction('program', 'instruction')
+    expect(node.isEqualNode(node2)).toBeTruthy()
+  })
+
+})
