df877b2e2ae26c0919b4ccd288ec701dd3695e30
Özgür Özçıtak <oozcitak@outlook.com>
2019-03-14 21:48:27 +0300

    Add ordered set tests

---
+++ test/dom/util/OrderedSet.test.ts
@@ -0,0 +1,26 @@
+import $$ from '../../TestHelpers'
+import { OrderedSet } from '../../../src/dom/util/OrderedSet';
+
+describe('OrderedSet', function () {
+
+  test('parse()', function () {
+    const set = OrderedSet.parse('a b c a')
+    expect(set.size).toBe(3)
+    const vals = ['a', 'b', 'c']
+    let i = 0
+    for (const val of set)
+    {
+      expect(val).toBe(vals[i++])
+    }
+  })
+
+  test('serialize()', function () {
+    const set = new Set(['a', 'b', 'c', 'a'])
+    expect(OrderedSet.serialize(set)).toBe('a b c')
+  })
+
+  test('sanitize()', function () {
+    expect(OrderedSet.sanitize('a  b   c   a')).toBe('a b c')
+  })
+
+})
