75b1dd8f4ccb2495547069f8eacad793e0a9e491
Ozgur Ozcitak <ozcitak@yahoo.com>
2019-06-13 16:52:41 +0300

    Add test for end function

---
+++ test/xmlbuilder/basic/end.test.ts
@@ -0,0 +1,31 @@
+import $$ from '../TestHelpers'
+
+describe('end()', () => {
+
+  test('simple document', () => {
+    const xml = $$.withOptions({ pubID: "pub", sysID: "sys" })
+      .create('root')
+      .com('comment')
+      .ins('target', 'content')
+      .dat('cdata node')
+      .txt('some text')
+      .ele('node', {att: 'val', att2: 'val2'})
+        .txt('more text')
+        .end()
+        
+    expect(xml).toBe(
+      '<?xml version="1.0"?>' +
+      '<!DOCTYPE root PUBLIC "pub" "sys">' +
+      '<root>' +
+        '<!--comment-->' +
+        '<?target content?>' +
+        '<![CDATA[cdata node]]>' +
+        'some text' +
+        '<node att="val" att2="val2">' +
+          'more text' +
+        '</node>' +
+      '</root>'
+      )
+  })
+
+})
