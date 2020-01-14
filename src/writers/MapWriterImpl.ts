3973d6d79f92a8aa351dbd02428206253b6e0fbc
Ozgur Ozcitak <ozcitak@yahoo.com>
2020-01-14 15:51:19 +0300

    Implement MapWriter using ObjectWriter

--- src/writers/MapWriterImpl.ts
+++ src/writers/MapWriterImpl.ts
@@ -1,23 +1,28 @@
 import {
-  XMLSerializedValue, MapWriterOptions, XMLBuilderOptions
+  XMLSerializedValue, MapWriterOptions, XMLBuilderOptions, ObjectWriterOptions
 } from "../builder/interfaces"
-import { applyDefaults } from "@oozcitak/util"
-import { 
-  Node, NodeType, CharacterData, ProcessingInstruction, Comment, Text, 
-  CDATASection, Element 
-} from "@oozcitak/dom/lib/dom/interfaces"
-import { PreSerializer } from "@oozcitak/dom/lib/serializer/PreSerializer"
-import { Guard } from "@oozcitak/dom/lib/util"
+import { applyDefaults, isArray, isObject, isMap, isPlainObject } from "@oozcitak/util"
+import { Node } from "@oozcitak/dom/lib/dom/interfaces"
+import { ObjectWriterImpl } from "./ObjectWriterImpl"
+
+type AttrNode = { "@": { [key: string]: string } }
+type TextNode = { "#": string | string[] }
+type CommentNode = { "!": string | string[] }
+type InstructionNode = { "?": string | string[] }
+type CDATANode = { "$": string | string[] }
+type NodeList = (ElementNode | AttrNode | TextNode | CommentNode |
+  InstructionNode | CDATANode)[]
+type ElementNode = { [key: string]: NodeList | NodeList[] }
 
 /**
- * Serializes XML nodes into maps and arrays.
+ * Serializes XML nodes into ES6 maps and arrays.
  */
 export class MapWriterImpl {
 
   private _builderOptions: XMLBuilderOptions
 
   /**
-   * Initializes a new instance of `MapWriterImpl`.
+   * Initializes a new instance of `ObjectWriterImpl`.
    * 
    * @param builderOptions - XML builder options
    */
@@ -36,7 +41,32 @@ export class MapWriterImpl {
       format: "map"
     })
 
-    return []
+    // convert to object
+    const objectWriterOptions: ObjectWriterOptions = applyDefaults(options, {
+      format: "object"
+    })
+    const objectWriter = new ObjectWriterImpl(this._builderOptions)
+    const val = objectWriter.serialize(node, objectWriterOptions)
+
+    // recursively convert object into Map
+    return this._convertObject(val)
+  }
+
+  _convertObject(obj: XMLSerializedValue): XMLSerializedValue {
+    if (isArray(obj)) {
+      for (let i = 0; i < obj.length; i++) {
+        obj[i] = this._convertObject(obj[i])
+      }
+      return obj
+    } else if (isObject(obj)) {
+      const map = new Map()
+      for (const key in obj) {
+        map.set(key, this._convertObject((obj as any)[key]))
+      }
+      return map
+    } else {
+      return obj
+    }
   }
 
 }
