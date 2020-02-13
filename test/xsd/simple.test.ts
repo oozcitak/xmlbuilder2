7c832c56408ae54396f989bb853b841ca3119eb2
Ozgur Ozcitak <ozcitak@yahoo.com>
2020-02-13 16:53:20 +0300

    Add libxml test

---
+++ test/xsd/simple.test.ts
@@ -0,0 +1,23 @@
+import $$ from '../TestHelpers'
+import { parseXml } from 'libxmljs'
+
+describe('Simple XSD validation', () => {
+
+  test('XSD validation', () => {
+    const xsdDoc = $$.create()
+      .ele('xs:schema', { 'xmlns:xs': 'http://www.w3.org/2001/XMLSchema' })
+        .ele('xs:element', { name: 'book' }).doc()
+    const xmlDoc1 = $$.create().ele('book').doc()
+    const xmlDoc2 = $$.create().ele('boob').doc()
+
+
+    const xsd = parseXml(xsdDoc.end() as string)
+    
+    const  xml1 = parseXml(xmlDoc1.end() as string)
+    const  xml2 = parseXml(xmlDoc2.end() as string)
+    
+    expect(xml1.validate(xsd)).toBe(true)
+    expect(xml2.validate(xsd)).toBe(false)
+  })
+  
+})
