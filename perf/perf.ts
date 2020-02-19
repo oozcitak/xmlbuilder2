5ba859b47d1b0c484bd450a6c6748b0f8b9dd5fa
Ozgur Ozcitak <ozcitak@yahoo.com>
2020-02-19 15:40:39 +0300

    Add medium document to performance cases

--- perf/perf.ts
+++ perf/perf.ts
@@ -11,24 +11,42 @@ import { readFileSync } from "fs"
 
 function createSmallDoc() {
   const root = create('root')
-  const node1 = root.ele('node1')
-  node1.ele('node1-1', {att1: "val1", att2: "val2" }, "text")
-  node1.ele('node1-2')
-  const node2 = root.ele('node2')
-  node2.ele('node2-1', {att1: "val1", att2: "val2" }, "text")
-  node2.ele('node2-2')
-  node2.ele('node2-3')
+  for (let i = 0; i < 100; i++) {
+    root.ele('node')
+      .ele('node1-1', {att1: "val1", att2: "val2" }, "text")
+      .ele('node1-2')
+  }
+  return root
 }
 
 function createSmallDoc2() {
   const root = create2().ele('root')
-  const node1 = root.ele('node1')
-  node1.ele('node1-1', {att1: "val1", att2: "val2" }).txt("text")
-  node1.ele('node1-2')
-  const node2 = root.ele('node2')
-  node2.ele('node2-1', {att1: "val1", att2: "val2" }).txt("text")
-  node2.ele('node2-2')
-  node2.ele('node2-3')
+  for (let i = 0; i < 100; i++) {
+    root.ele('node')
+      .ele('node1-1', {att1: "val1", att2: "val2" }).txt("text")
+      .ele('node1-2')
+  }
+  return root
+}
+
+function createMediumDoc(): any {
+  const root = create('root')
+  for (let i = 0; i < 10000; i++) {
+    root.ele('node')
+      .ele('node1-1', {att1: "val1", att2: "val2" }, "text")
+      .ele('node1-2')
+  }
+  return root
+}
+
+function createMediumDoc2(): any {
+  const root = create2().ele('root')
+  for (let i = 0; i < 10000; i++) {
+    root.ele('node')
+      .ele('node1-1', {att1: "val1", att2: "val2" }).txt("text")
+      .ele('node1-2')
+  }
+  return root
 }
 
 (function () {
@@ -36,6 +54,17 @@ function createSmallDoc2() {
    
   suite.add("xmlbuilder", () => createSmallDoc())
   suite.add("xmlbuilder2", () => createSmallDoc2())
+
+  suite.on("complete", () => processBenchmark(suite, "xmlbuilder2"))
+  suite.run()
+
+})();
+
+(function () {
+  const suite = new Suite("medium document with ele()")
+   
+  suite.add("xmlbuilder", () => createMediumDoc())
+  suite.add("xmlbuilder2", () => createMediumDoc2())
 
   suite.on("complete", () => processBenchmark(suite, "xmlbuilder2"))
   suite.run()
@@ -62,6 +91,20 @@ const smallObj = JSON.parse(str);
   
   const doc = create("root").ele(smallObj).doc()
   const doc2 = create2().ele("root").ele(smallObj).doc()
+
+  suite.add("xmlbuilder", () => doc.end({ pretty: true }))
+  suite.add("xmlbuilder2", () => doc2.end({ prettyPrint: true }))
+
+  suite.on("complete", () => processBenchmark(suite, "xmlbuilder2"))
+  suite.run()
+
+})();
+
+(function () {
+  const suite = new Suite("serialize medium document to XML string")
+  
+  const doc = createMediumDoc().doc()
+  const doc2 = createMediumDoc2().doc()
 
   suite.add("xmlbuilder", () => doc.end({ pretty: true }))
   suite.add("xmlbuilder2", () => doc2.end({ prettyPrint: true }))
