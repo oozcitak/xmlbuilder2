a3e552796df1fe5de3d31af07bb9114eb031c35c
Ozgur Ozcitak <ozcitak@yahoo.com>
2020-03-02 13:05:29 +0300

    Move expect into end

--- test/stream/async.test.ts
+++ test/stream/async.test.ts
@@ -10,22 +10,24 @@ const readFile = promisify(fs.readFile)
 
 describe('Use callback API with fs async', () => {
 
-  test('basic', async () => {
+  test('basic', async (done) => {
     const filename = resolve(__dirname, 'async-basic.test.out')
     const outFile = await open(filename, 'w')
     
     const xml = $$.createCB({ 
-      data: async (chunk) => await write(outFile, chunk),
-      end: async () => await close(outFile)
+      data: (chunk) => write(outFile, chunk),
+      end: async () => {
+        await close(outFile)
+        const result = await readFile(filename, { encoding: 'utf8' })
+        expect(result).toBe(`<root><foo/><bar fizz="buzz"/></root>`)
+        done()
+      }
     })
 
     xml.ele("root")
       .ele("foo").up()
       .ele("bar").att("fizz", "buzz").up()
       .end()
-
-    const result = await readFile(filename, { encoding: 'utf8' })
-    expect(result).toBe(`<root><foo/><bar fizz="buzz"/></root>`)
   })
 
 })
