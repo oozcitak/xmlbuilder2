0b9b56807b9e5dfe66116bd3f6776abd668b2303
Ozgur Ozcitak <ozcitak@yahoo.com>
2020-03-02 14:02:44 +0300

    Move async tests into end callback

--- test/wiki/functions-callback.test.ts
+++ test/wiki/functions-callback.test.ts
@@ -10,40 +10,44 @@ const readFile = promisify(fs.readFile)
 
 describe('examples in the callback function reference wiki page', () => {
 
-  test('documentCB()', async () => {
+  test('documentCB()', async (done) => {
     const filename = resolve(__dirname, 'functions-documentCB.test.out')
     const outFile = await open(filename, 'w')
     
     const xmlStream = $$.createCB({ 
       data: async (chunk) => await write(outFile, chunk),
-      end: async () => await close(outFile)
+      end: async () => {
+        await close(outFile)
+        const result = await readFile(filename, { encoding: 'utf8' })
+        expect(result).toBe('<root><foo/><bar fizz="buzz"/></root>')
+        done()
+      }
     })
     
     xmlStream.ele("root")
       .ele("foo").up()
       .ele("bar").att("fizz", "buzz").up()
       .end()
-
-    const result = await readFile(filename, { encoding: 'utf8' })
-    expect(result).toBe('<root><foo/><bar fizz="buzz"/></root>')
   })
 
-  test('fragmentCB()', async () => {
+  test('fragmentCB()', async (done) => {
     const filename = resolve(__dirname, 'functions-fragmentCB.test.out')
     const outFile = await open(filename, 'w')
     
     const xmlStream = $$.fragmentCB({ 
       data: async (chunk) => await write(outFile, chunk),
-      end: async () => await close(outFile)
+      end: async () => {
+        await close(outFile)
+        const result = await readFile(filename, { encoding: 'utf8' })
+        expect(result).toBe('<foo/><foo fizz="buzz"/><foo/>')
+        done()
+      }
     })
     
     xmlStream.ele("foo").up()
       .ele("foo").att("fizz", "buzz").up()
       .ele("foo").up()
       .end()
-
-    const result = await readFile(filename, { encoding: 'utf8' })
-    expect(result).toBe('<foo/><foo fizz="buzz"/><foo/>')
   })
 
 })
