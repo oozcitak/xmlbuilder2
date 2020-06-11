376faa9bfb2bdd84e61fc48cd6c2538ea84defdf
Özgür ÖZÇITAK <ozgurozcitak@ttsmuhendislik.local>
2020-06-11 12:49:51 +0300

    Update test for #20

--- test/issues/issue-020.test.ts
+++ test/issues/issue-020.test.ts
@@ -3,18 +3,30 @@ import $$ from "../TestHelpers";
 describe("Replicate issue", () => {
   // https://github.com/oozcitak/xmlbuilder2/issues/20
   test("#20 - Setting xmlns causes problem on children", () => {
-    const node = $$.create().ele('root').ele('child').txt('child text')
+    const node = $$.create({ root: { child: 'child text' } })
+    const node2 = $$.create().ele('root').ele('child').txt('child text').up().up();
+    
     expect(node.end({ prettyPrint: true, headless: true })).toBe($$.t`
     <root>
       <child>child text</child>
     </root>
     `)
+    expect(node.root().att('xmlns', 'anything').end({ prettyPrint: true, headless: true })).toBe($$.t`
+    <root xmlns="anything">
+      <child>child text</child>
+    </root>
+    `)
 
-    node.root().att('xmlns', 'anything')
-    expect(node.end({ prettyPrint: true, headless: true })).toBe($$.t`
+    expect(node2.end({ prettyPrint: true, headless: true })).toBe($$.t`
+    <root>
+      <child>child text</child>
+    </root>
+    `)
+    expect(node2.root().att('xmlns', 'anything').end({ prettyPrint: true, headless: true })).toBe($$.t`
     <root xmlns="anything">
       <child>child text</child>
     </root>
     `)
   })
+
 })
