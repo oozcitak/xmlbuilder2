017cf85f2ed6924431b84a802749312ddfc9e997
Özgür Özçıtak <ozcitak@yahoo.com>
2020-06-22 12:09:50 +0300

    Also check element namespaces

--- test/issues/issue-025.test.ts
+++ test/issues/issue-025.test.ts
@@ -10,13 +10,17 @@ describe("Replicate issue", () => {
             'ns:node2': 1234
         }  
     }
+    const doc = $$.create(obj);
     
-    expect($$.create(obj).end({ headless: true, prettyPrint: true })).toBe($$.t`
+    expect(doc.end({ headless: true, prettyPrint: true })).toBe($$.t`
       <root xmlns:ns="some/uri">
         <ns:node1>Some text</ns:node1>
         <ns:node2>1234</ns:node2>
       </root>
     `)
+
+    expect((doc.root().first().node as any).namespaceURI).toBe('some/uri')
+    expect((doc.root().last().node as any).namespaceURI).toBe('some/uri')
   })
 
 })
