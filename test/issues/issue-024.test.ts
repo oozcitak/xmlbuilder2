64650d6a78284c3195e7dd2a7d1b69f5a761778f
Özgür Özçıtak <ozcitak@yahoo.com>
2020-06-22 11:01:02 +0300

    Fix typo

--- test/issues/issue-024.test.ts
+++ test/issues/issue-024.test.ts
@@ -2,7 +2,7 @@ import $$ from "../TestHelpers";
 
 describe("Replicate issue", () => {
   // https://github.com/oozcitak/xmlbuilder2/issues/24
-  test("#24 - \r encoding in element txt ", () => {
+  test("#24 - \r encoding in element txt", () => {
     const root = $$.create().ele('parent', {
       text: 'Hello \r\n'
     }).ele('child').txt('Line 1\r\nLine 2\r\nLine 3')
