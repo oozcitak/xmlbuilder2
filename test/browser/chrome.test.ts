995c6f8c42a021131778aaacbb2ff81fd6e39a0a
Özgür Özçıtak <ozcitak@yahoo.com>
2020-07-01 13:33:22 +0300

    Add chrome tests

--- test/browser/chrome.test.ts
+++ test/browser/chrome.test.ts
@@ -1,8 +1,9 @@
-import { Builder, WebDriver, Capabilities } from 'selenium-webdriver'
+import { Builder, WebDriver, Capabilities, By } from 'selenium-webdriver'
 import { ServiceBuilder, setDefaultService } from 'selenium-webdriver/chrome'
 import { path as chromePath } from 'chromedriver'
-import { resolve, join } from "path"
-describe('object', () => {
+import { resolve, join } from 'path'
+
+describe('chrome', () => {
   
   let driver: WebDriver
 
@@ -18,16 +19,13 @@ describe('object', () => {
 
   afterAll(async () => {
     await driver.quit()
-  })
+  }, 10000)
 
-  beforeEach(async () => {
+  test('browser tests', async () => {
     const path = resolve(join(__dirname, 'test.html'))
     await driver.get(path)
-  })
-
-  test('from JS object with decorators', async () => {
-    const result = await driver.executeScript(`var root = xmlbuilder2.create().ele('root'); return root.end();`)
-    expect(result).toBe(`<?xml version=\"1.0\"?><root/>`)
-  })
+    const element = await driver.findElement(By.id('test-result'))
+    return expect(element.getText()).resolves.toBe('PASS')
+  }, 10000)
 
 })
