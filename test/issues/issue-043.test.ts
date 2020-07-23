3f0f716d8d3bc93833904f1effe848cd227044a2
Özgür Özçıtak <ozcitak@yahoo.com>
2020-07-23 11:52:36 +0300

    Add test for #43

---
+++ test/issues/issue-043.test.ts
@@ -0,0 +1,30 @@
+import $$ from '../TestHelpers'
+import { select } from "xpath"
+
+describe('Replicate issue', () => {
+
+  // https://github.com/oozcitak/xmlbuilder2/issues/43
+  test('#43 - Add an Element (from XPath query) to xmlbuilder2 object', () => {
+    const doc1 = $$.create()
+      .ele("records")
+        .ele("record", { id: 1 }).up()
+        .ele("record", { id: 2 }).up()
+      .doc()
+
+    const doc2 = $$.create()
+      .ele("results")
+      .doc()
+     
+    const result = select("//record", doc1.node as any)
+    if (result && result.length && result.length !== 0) {
+      const recordNode = $$.builder(result[0] as any)
+      doc2.root().import(recordNode)
+    }
+    expect(doc2.end({ prettyPrint: true })).toBe($$.t`
+    <?xml version="1.0"?>
+    <results>
+      <record id="1"/>
+    </results>
+    `)
+  })
+})
