e80c99c83592f9248ccd747d19ead13ba4a004e0
Özgür Özçıtak <ozcitak@yahoo.com>
2020-08-04 17:51:13 +0300

    Add test for #45

---
+++ test/issues/issue-045.test.ts
@@ -0,0 +1,47 @@
+import $$ from "../TestHelpers";
+import { XMLBuilder } from "../../src/interfaces";
+
+describe("Replicate issue", () => {
+  // https://github.com/oozcitak/xmlbuilder2/issues/45
+  test("#45 - How to convert an Json Array into Xml Array like this", () => {
+    const obj = {
+      'root': {
+        'items': [
+          { '@id': 1 },
+          { '@id': 2 },
+          { '@id': 3 }
+        ]
+      }
+    }
+
+    const elementParser = function (parent: XMLBuilder, namespace: any, name: string) {
+      // check if we have a plural name
+      if (name.endsWith('s')) {
+        // check if we have a parent node with the same name
+        // otherwise create one
+        let itemsNode = parent.find(n => n.node.nodeName === name)
+        if (itemsNode === undefined) {
+          itemsNode = parent.ele(name)
+        }
+
+        let childName = name.slice(0, -1)
+        return itemsNode.ele(childName)
+      } else {
+        return parent.ele(name)
+      }
+    }
+    
+    const doc = $$.create({ parser: { element: elementParser } }, obj)
+
+    expect(doc.end({ headless: true, prettyPrint: true })).toBe($$.t`
+      <root>
+        <items>
+          <item id="1"/>
+          <item id="2"/>
+          <item id="3"/>
+        </items>
+      </root>
+    `)
+  })
+
+})
