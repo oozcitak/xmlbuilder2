ed0a43fd30695396b1b8df3d77aab25e3613bd44
Özgür Özçıtak <ozcitak@yahoo.com>
2020-08-04 14:47:11 +0300

    Add test for custom XML parser

--- test/readers/custom.test.ts
+++ test/readers/custom.test.ts
@@ -32,4 +32,14 @@ describe('custom parsers', () => {
       `)
   })
 
+  test('XMLReader - skip DocType', () => {
+    const xml = $$.t`
+    <?xml version="1.0"?>
+    <!DOCTYPE root PUBLIC "pub" "sys">
+    <root xmlns="ns"/>`
+
+    const doc = $$.create({ parser: { docType: () => undefined } }, xml).doc()
+    expect(doc.end()).toBe('<?xml version="1.0"?><root xmlns="ns"/>')
+  })
+
 })
