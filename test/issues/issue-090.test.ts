fcc16d95c89b618c1de10cc7db0a9e5ab946e490
oozcitak <oozcitak@gmail.com>
2021-07-09 18:05:09 +0300

    Add test for issue #90

---
+++ test/issues/issue-090.test.ts
@@ -0,0 +1,25 @@
+import $$ from "../TestHelpers";
+
+describe("Replicate issue", () => {
+  // https://github.com/oozcitak/xmlbuilder2/issues/90
+  test(`#90 - The Mixed Content example for Text Object Conversion does not work.`, () => {
+    const obj2 = {
+      monologue: {
+        '#': [
+          'Talk to me Goose!',
+          { 'cut': 'dog tag shot' },
+          'Talk to me...'
+        ]
+      }
+    };
+    const doc = $$.create().ele(obj2);
+    expect(doc.end({ prettyPrint: true})).toBe(`
+    <monologue>
+      Talk to me Goose!
+      <cut>dog tag shot</cut>
+      Talk to me...
+    </monologue>
+    `);
+  })
+
+})
